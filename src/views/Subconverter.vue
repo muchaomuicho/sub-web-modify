d29afa8bbe0006db660c85a96c6dba4dacc618b3
WuCNebssss <muchaomuicho@gmail.com>
2025-07-02 08:49:36 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -793,7 +793,7 @@ export default {
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
         includeRemarks: "",
-        filename: "转换",
+        filename: "转换", //订阅命名
         rename: "", //节点命名
         devid: "",
         interval: "",
@@ -806,7 +806,7 @@ export default {
         tfo: false,
         sort: false, //基础节点排序
         expand: true,
-        scv: true,
+        scv: true, //跳过证书验证
         fdn: false,
         appendType: false,
         insert: false, // 是否插入默认订阅的节点，对应配置项 insert_url
